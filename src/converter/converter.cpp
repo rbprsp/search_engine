b0fedaf0b8e820e0b1f919f6370ba230e69f38f0
szmxnv <maillikelol@gmail.com>
2024-05-19 15:21:07 +0500

    fully rewrite class logic & removed parser class

--- src/converter/converter.cpp
+++ src/converter/converter.cpp
@@ -6,47 +6,151 @@
 
 using nlohmann::json;
 
-std::vector<std::string> ConverterJSON::GetTextFromDocs()
+ConverterJSON::ConverterJSON()
 {
-    std::vector<std::string> textFromDocs;
+    this->file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/logs.txt", true);
+    this->logger = std::make_shared<spdlog::logger>("ConverterJSON", this->file_sink);
+
+    spdlog::set_default_logger(this->logger);
+
+    if(this->validateConfig())
+    {
+        if(!this->readAllDocs() || !this->readRequests() || !this->readResponLimit())
+        {
+            logger->critical("Error while parsing .json file."); 
+            throw std::runtime_error("Unable to read configuration file.");
+        }
+    }
+    else
+    {
+       logger->critical("Can't find config path | Can't validate .json format in config file."); 
+       throw std::runtime_error("Unable to read configuration file.");
+    }
+}
+
+bool ConverterJSON::validateConfig()
+{
     json j;
-    std::ifstream file(this->configFileName);
+    std::ifstream file(this->config_file_name);
 
     if(!file.is_open())
-        throw std::runtime_error("Unable to open config file.");
+    {
+        logger->critical("Unable to open config file.");
+        return false;
+    }
+
+    logger->info("Config file was found.");
+
+    try
+    {
+        file >> j;
+        file.close();
+        logger->info("Config file read successfully.");
+    }
+    catch(const std::exception& e)
+    {
+        logger->critical("Invalid .json format in config.json. Error: {}.", e.what());
+        file.close();
+        return false;
+    }
+
+    return true;
+}
+
+bool ConverterJSON::readAllDocs()
+{
+    json j;
+    std::ifstream file(this->config_file_name);
+
+    namespace fs = std::filesystem;
+    if(!j["files"].empty())
+    {
+        size_t file_counter = 0;
+        for(std::string doc_id : j["files"])
+        {
+            fs::path doc = doc_id;
+            if(!fs::exists(doc))
+            {
+                logger->warn("File not exists.");
+                continue;
+            }
+            else
+            {
+                logger->info("Working on file.");
+                std::ifstream file(doc);
+                if(!file.is_open())
+                {
+                    logger->warn("Unable to open file.");
+                    continue;
+                }
+                else
+                {
+                    std::string line = {}, content = {};
+                    while(std::getline(file, line))
+                        content += line + "\n";
+
+                    this->docs.push_back(content);
+                    file_counter++;
+                    file.close();
+                }
+            }
+        }
+
+        if(file_counter == 0)
+        {
+            logger->critical("No valid input files.");
+            return false;
+        }
+    }
+    else
+    {
+        logger->critical("No input files.");
+        return false;
+    }
+
+    logger->info("Text from files was successfully added.");
+    return true;
+}
+
+bool ConverterJSON::readResponLimit()
+{
+    json j;
+    std::ifstream file(this->config_file_name);
+
     file >> j;
     file.close();
 
-    for(std::string doc : j["files"])
+    if(!j["max_responses"].empty())
     {
-        std::string line, content;
-        std::ifstream file(doc);
-
-        if(!file.is_open())
-            throw std::runtime_error("Unable to open document. Path: " + doc);
-
-        while(std::getline(file, line))
-            content += line + "\n";
-
-        textFromDocs.push_back(content);
-        file.close();
+        try
+        {
+            this->max_responses = j["max_responses"].get<int>();
+            logger->info("max_responses value parsed successfully.");
+            return true;
+        }
+        catch(const std::exception& e)
+        {
+            logger->critical("Error while parsing max_reponses value. Error {}.", e.what());
+            return false;   
+        }
     }
 
-    return textFromDocs;
+    return true;
+}
+
+bool ConverterJSON::readRequests()
+{
+    return true;
+}
+
+std::vector<std::string> ConverterJSON::GetTextFromDocs()
+{
+    return this->docs;
 }
 
 int ConverterJSON::GetResponsesLimit()
 {
-    json j;
-
-    std::ifstream file(this->configFileName);
-    if(!file.is_open())
-        throw std::runtime_error("Unable to open config file.");
-
-    file >> j;
-    file.close();
-
-    return j["max_responses"].get<int>();
+    return this->max_responses;
 }
 
 std::vector<std::string> ConverterJSON::GetRequests()
--- src/converter/converter.h
+++ src/converter/converter.h
@@ -4,6 +4,11 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <memory>
+
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
+
 
 struct Entry
 {
@@ -14,12 +19,26 @@ struct Entry
 class ConverterJSON
 {
 private:
-    const std::string configFileName = "config.json";
+    //config
+    const std::string config_file_name = "config.json";
+
+    //data from files
     std::vector<std::string> docs;
-    std::map<std::string, std::vector<Entry>> freqDictionary;
+    std::map<std::string, std::vector<Entry>> freq_dictionary;
+    int max_responses = 0;
+
+    //logger
+    std::shared_ptr<spdlog::sinks::basic_file_sink_mt> file_sink;
+    std::shared_ptr<spdlog::logger> logger;
+
+    bool validateConfig();
+
+    bool readAllDocs();
+    bool readResponLimit();
+    bool readRequests();
 
 public:
-    ConverterJSON() = default;
+    ConverterJSON();
 
     std::vector<std::string> GetTextFromDocs();
 
